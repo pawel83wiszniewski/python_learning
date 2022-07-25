b5aa9e7f3a48c90cc68fd5ea09c751beb12f81d2
pawel83wiszniewski <71722987+pawel83wiszniewski@users.noreply.github.com>
2022-07-25 04:12:52 +0200

    Python Learning

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-print('Paweł')
-print('o----')
-print('||||')
-print('*' * 10)
+# price = 10
+# rating = 4.9
+# name = "Paweł"
+# is_published = False
+# print(price)
+#
+# full_name = "Jacek Jakiś"
+# age = 20
+# is_new = True
+#
+# name = input('Jak masz na imię? ')
+# print('Siemka ' + name)
+# favorite_color = input('Jaki jest Twój ulubiony kolor? ')
+# print('Paweł lubi ' + favorite_color)
+
+# birth_year = input('Rok urodzenia: ')
+# print(type(birth_year))
+# age = 2022 - int(birth_year)
+# print(type(age))
+# print(age)
+
+# weight_lbs = input('Weight (lbs): ')
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+
+# a = 5 #int
+# b = 4.3 #float
+# imie = "Paweł" #string
+# czyDuzy = True #boolean (prawda lub fałsz)
+
